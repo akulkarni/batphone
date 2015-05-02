5ab47e0a530ee8a8d36b46c6d0830c1a48c2fed5
ajay <ajay@watchvast.com>
2015-05-02 12:34:39 -0400

    test

---
+++ init.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+module BatConfig
+
+  def get_main_number
+  	return '+14402021404'
+  end
+
+  def get_main_members
+  	return ['+19175731568']
+  end
+
+  def get_conference_id
+    return 'Batphone'
+  end
+  
+  def get_host
+  	return 'https://bat-phone-440.herokuapp.com'
+  end
+
+  def get_twilio_client
+    account_sid = "AC2c0c745ec4d44b2e8c34ce702d81dadd"
+    auth_token = "4c8d9d87c5e4b1f0634a6a27e9bc9300"
+    return Twilio::REST::Client.new account_sid, auth_token
+  end
+
+end
