54a99a8875fde1376196a681b32011f72d49484c
ajay <ajay@watchvast.com>
2015-05-02 00:29:36 -0400

    test

--- app.rb
+++ app.rb
@@ -1,12 +1,25 @@
 # app.rb
 require 'sinatra'
+require 'twilio-ruby'
 
 class App < Sinatra::Base
   configure :production, :development do
     enable :logging
   end
 
+  @account_sid = "AC2c0c745ec4d44b2e8c34ce702d81dadd"
+  @auth_token = "4c8d9d87c5e4b1f0634a6a27e9bc9300"
+
   get '/' do
   	'Bat man!'
   end
+
+  post '/call?' do
+    puts params
+  end
+
+  post '/sms?' do
+    puts params
+  end
+
 end 
