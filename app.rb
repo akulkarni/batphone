a388a7e46881cd262e6d1d6e5ce6d0e2424364de
ajay <ajay@watchvast.com>
2015-05-02 12:27:08 -0400

    test

--- app.rb
+++ app.rb
@@ -2,12 +2,8 @@
 require 'sinatra'
 require 'twilio-ruby'
 
-## TODO
-# If a conference is already in progress, should you let caller in?
-# PRO: If someone drops out, can easily rejoin
-# CON: New caller will be able to listen in -- can't give same phone number to multiple people
-### ONLY LET SOMEONE (NOT EXISTING PART) TO JOIN IF THEY WERE LAST PERSON OR IF PARTICIPANTS ONLY MEMBERS
-# IF ALREADY 3 MEMBERS, PLEASE CALL AGAIN LATER
+require_relative 'init'
+helpers BatConfig
 
 class App < Sinatra::Base
   configure :production, :development do
@@ -97,28 +93,6 @@ class App < Sinatra::Base
         "Sorry, we are currently on a call. We'll call you back."
       end
     end
-  end
-
-  def get_main_number
-  	return '+14402021404'
-  end
-
-  def get_main_members
-  	return ['+19175731568', '+19735680605']
-  end
-
-  def get_conference_id
-    return 'Batphone'
-  end
-  
-  def get_host
-  	return 'https://bat-phone-440.herokuapp.com'
-  end
-
-  def get_twilio_client
-    account_sid = "AC2c0c745ec4d44b2e8c34ce702d81dadd"
-    auth_token = "4c8d9d87c5e4b1f0634a6a27e9bc9300"
-    return Twilio::REST::Client.new account_sid, auth_token
   end
 
 end 
