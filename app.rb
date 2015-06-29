b0edec88178585109447bb7b2db494426e68dad4
ajay <ajay@watchvast.com>
2015-06-29 19:12:09 -0400

    testing sleep for incoming call

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class App < Sinatra::Base
 
       # Pause for repeated intervals waiting for others to join
       i = 0
-      while (i < 4)
+      while (i < 5)
         sleep(3)
         if active_conferences.size > 0
           i = 100
